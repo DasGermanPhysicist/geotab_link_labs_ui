dcca6689b62a056704a548787431daea474789f9
tsteinholz <thomas.steinholz@link-labs.com>
2025-01-30 16:15:35 -0500

    update base url to match repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/GeotabAdd-In/', // Set this to your repository name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
