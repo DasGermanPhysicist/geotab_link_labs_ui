f46331a20b629bc6dd878209b4af1dd4fe8fc1d1
tsteinholz <thomas.steinholz@link-labs.com>
2025-01-30 23:53:37 -0500

    add debug logs

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,11 @@ import App from './App.tsx';
 import './index.css';
 import { GeotabLifecycle } from './lib/GeotabLifecycle';
 
+console.log("Hello World!")
+
 // Check if 'geotab' is defined before adding the add-in
 if (typeof geotab !== 'undefined') {
+  console.log("Running in Geotab Platform")
   geotab.addin.AirfinderAddIn = GeotabLifecycle();
 } else {
   console.warn("Not running in Geotab Platform")
