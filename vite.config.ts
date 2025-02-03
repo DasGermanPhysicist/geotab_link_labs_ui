f9a6a2c60ad75e2afc8d709c32f382c3158244ec
tsteinholz <thomas.steinholz@link-labs.com>
2025-02-03 17:17:34 -0500

    use correct addin config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL || '/',
+  base: process.env.ADDIN_BASE_URL || '/',
   // base: 'https://geotab.link-labs.com',
   optimizeDeps: {
     exclude: ['lucide-react'],
